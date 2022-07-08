84f3d49d221f49729131b34cd0cd1ec5f60299b2
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-08 17:22:50 -0300

    added rabbitmq consumer

--- src/index.js
+++ src/index.js
@@ -3,28 +3,41 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from "body-parser";
 
-const app = express();
+import RabbitmqServer from './factory/rabbitmq_server'
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
+(async () => {
+  const app = express();
 
-process.on('uncaughtException', function (error) {
-  console.log(error.stack);
-  console.log("uncaughtException Node NOT Exiting...");
-});
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(express.static('public'));
+  
+  process.on('uncaughtException', function (error) {
+    console.log(error.stack);
+    console.log("uncaughtException Node NOT Exiting...");
+  });
+  
+  const port = process.env.PORT || 4001;
+  
+  app.get('/welcome', (req, res, next) => {
+    res.status(200).send({ message: 'Welcome' })
+  });
+  
+  app.post('/welcome', (req, res, next) => {
+    res.status(200).send(req.body)
+  });
+  
+  app.listen(port, async () => {
+    console.log(`Server started at port ${port}`);
+    await consumer();
+  });
 
-const port = process.env.PORT || 4001;
-
-app.get('/welcome', (req, res, next) => {
-  res.status(200).send({ message: 'Welcome' })
-});
-
-app.post('/welcome', (req, res, next) => {
-  res.status(200).send(req.body)
-});
-
-app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
-});
+  const consumer = async () => {
+    const server = new RabbitmqServer(process.env.RABBITMQ_SERVER_HOST);
+    await server.start();
+    await server.consume('notifications', (message) => console.log(message.content.toString()));
+  }
+  
+  
+})();
