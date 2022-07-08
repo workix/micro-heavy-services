0cfc75b83ea988188e97d6f41db9154ff3f1fb88
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-07-08 17:11:56 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+process.on('uncaughtException', function (error) {
+  console.log(error.stack);
+  console.log("uncaughtException Node NOT Exiting...");
+});
+
+const port = process.env.PORT || 4001;
+
+app.get('/welcome', (req, res, next) => {
+  res.status(200).send({ message: 'Welcome' })
+});
+
+app.post('/welcome', (req, res, next) => {
+  res.status(200).send(req.body)
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
